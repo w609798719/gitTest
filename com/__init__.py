4f31d36528d2969e2687d1b27acf21084a40c6b2
w609798719 <609798719@qq.com>
2019-05-13 22:26:48 +0800

    Initial commit

---
+++ com/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# module __init__.py wal
+# Created on 2019/5/13 22:21
---
+++ com/gitTest/first/HelloWorld.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# module HelloWorld wal
+# Created on 2019/5/13 22:21
+
+a = 123
+print(a)
